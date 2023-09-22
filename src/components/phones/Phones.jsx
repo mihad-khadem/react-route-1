065c2552fb2c3c026419acf54d94bc27fb4c09a7
mihad-khadem <mihadkhadem@gmail.com>
2023-09-23 02:45:04 +0600

    Exlpored Awesome React components/spin loader

--- src/components/phones/Phones.jsx
+++ src/components/phones/Phones.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+import { Bars } from  'react-loader-spinner'
+
 import axios from "axios";
+
+
+
+
 const Phones = () => {
     const [phones, setPhones] = useState([])
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
 
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -19,6 +26,7 @@ const Phones = () => {
             })
             setPhones(phonesWithFakeData);
             console.log(phonesWithFakeData);
+            setLoading(false)
         })
         
 
@@ -29,6 +37,17 @@ const Phones = () => {
     }, [])
     return (
         <div>
+            {
+                loading && <Bars
+                height="80"
+                width="80"
+                color="#4fa94d"
+                ariaLabel="bars-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+              />
+            }
             <h2 className="text-2xl font-bold text-center mt-4">Phones : {phones.length}s</h2>
             <div>
                 <BarChart width={600} height={400} data={phones}>
